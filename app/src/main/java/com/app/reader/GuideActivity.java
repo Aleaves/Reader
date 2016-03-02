683eb9fed2642c7dfb5f95ee59bcdb1150fe3261
Aleaves <2849459897@qq.com>
2016-03-02 18:16:13 +0800

    欢迎页侧滑功能demo

---
+++ app/src/main/java/com/app/reader/GuideActivity.java
@@ -0,0 +1,88 @@
+package com.app.reader;
+
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.reader.View.CircleIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by llb on 2016/3/2.
+ */
+public class GuideActivity extends AppCompatActivity{
+    private List<View> viewList;
+    private ViewPager viewPager;
+    private CircleIndicator circleIndicator;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avtivity_guide);
+        initData();
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(pagerAdapter);
+
+        circleIndicator = (CircleIndicator) findViewById(R.id.indicator);
+        circleIndicator.setViewPager(viewPager);
+    }
+
+    private void initData(){
+        viewList = new ArrayList<View>();
+        Random random = new Random();
+        for(int i=0;i<5;i++){
+            View view = new View(this);
+            view.setBackgroundColor(0xff000000| random.nextInt(0x00ffffff));
+            viewList.add(view);
+        }
+    }
+
+    PagerAdapter pagerAdapter = new PagerAdapter() {
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+
+            return arg0 == arg1;
+        }
+
+        @Override
+        public int getCount() {
+
+            return viewList.size();
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position,
+                                Object object) {
+            container.removeView(viewList.get(position));
+
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+
+            return super.getItemPosition(object);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            return "title";
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(viewList.get(position));
+
+            return viewList.get(position);
+        }
+
+    };
+
+}
