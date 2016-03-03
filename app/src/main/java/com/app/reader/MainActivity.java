87f5f2c37b919ada3af2417ef9a1b478e20e8f53
Aleaves <2849459897@qq.com>
2016-03-03 16:16:32 +0800

    测试

--- app/src/main/java/com/app/reader/MainActivity.java
+++ app/src/main/java/com/app/reader/MainActivity.java
@@ -1,11 +1,16 @@
 package com.app.reader;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("MainActicity","b哥威武");
+        Log.e("MainActicity","还是不行啊");
     }
 }
