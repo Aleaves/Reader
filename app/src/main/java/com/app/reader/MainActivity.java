c944f008f108f239ead01344d75bb5e332246117
Aleaves <2849459897@qq.com>
2016-03-04 16:12:40 +0800

    wwww

--- app/src/main/java/com/app/reader/MainActivity.java
+++ app/src/main/java/com/app/reader/MainActivity.java
@@ -1,12 +1,14 @@
 package com.app.reader;
 
+import android.util.Log;
+
 public class MainActivity extends BaseActivity {
 
 
     @Override
     protected void init() {
-        Log.e("test","hzp.s..........");
-        Log.i("=========","=========");
+        Log.e("test", "hzp.s..........");
+        Log.e("test", "hzp.wwwwwwwwwww..........");
     }
 
     @Override
