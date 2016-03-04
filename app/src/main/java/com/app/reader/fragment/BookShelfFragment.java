7e716e1419ca06dbdcf5b33b6ba8c4460cb7cbaf
Aleaves <2849459897@qq.com>
2016-03-04 16:00:47 +0800

    底部导航

---
+++ app/src/main/java/com/app/reader/fragment/BookShelfFragment.java
@@ -0,0 +1,22 @@
+package com.app.reader.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.reader.R;
+
+/**
+ * Created by llb on 2016/3/4.
+ */
+public class BookShelfFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_book_shelf,container,false);
+        return view;
+    }
+}
